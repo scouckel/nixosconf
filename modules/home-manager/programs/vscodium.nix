78c7cb8671443e46df40636a1b604dff74e0b62d
scouckel <james.krinsky@gmail.com>
2025-07-02 00:53:58 -0400

    added vscodium extensions

--- modules/home-manager/programs/vscodium.nix
+++ modules/home-manager/programs/vscodium.nix
@@ -7,7 +7,12 @@
 
   profiles.default.extensions = with pkgs.vscode-extensions; [
     jnoortheen.nix-ide
-    vscode-icons-team.vscode-icons
+    pkief.material-icon-theme
+    eamodio.gitlens
+    wix.vscode-import-cost
+    ms-vscode-remote.remote-ssh
+    tomoki1207.pdf
+    mechatroner.rainbow-csv
   ];
   };
 }
