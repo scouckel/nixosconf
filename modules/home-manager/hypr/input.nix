d2df0a049a3d143d5492dde5bbd444d37adc343c
scouckel <james.krinsky@gmail.com>
2025-08-08 17:08:21 -0400

    per-device?

--- modules/home-manager/hypr/input.nix
+++ modules/home-manager/hypr/input.nix
@@ -1,7 +1,16 @@
 {
-  wayland.windowManager.hyprland.settings = {
-    input = {
-      sensitivity = -0.7;
+  wayland.windowManager.hyprland = {
+    settings = {
+      input = {
+        sensitivity = -0.7;
+      };
     };
+
+    extraConfig = ''
+      device {
+        name = "razer-razer-deathadder-v2-x-hyperspeed"
+        sensitivity = -0.7
+      }
+    '';
   };
 }
