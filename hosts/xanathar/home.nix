9acb67798ddddf73e67dd939023babe388210b5d
scouckel <james.krinsky@gmail.com>
2025-08-08 16:47:16 -0400

    xanathar monitor

--- hosts/xanathar/home.nix
+++ hosts/xanathar/home.nix
@@ -59,6 +59,25 @@
     # EDITOR = "emacs";
   };
 
+wayland.windowManager.hyprland.settings = {
+    monitor = [
+      "eDP-1, 1920x1080@60, 0x0, 1"
+    ];
+
+    workspace = [
+      "1,monitor:eDP-1"
+      "2,monitor:eDP-1"
+      "3,monitor:eDP-1"
+      "4,monitor:eDP-1"
+      "5,monitor:eDP-1"
+      "6,monitor:eDP-1"
+      "7,monitor:eDP-1"
+      "8,monitor:eDP-1"
+      "9,monitor:eDP-1"
+      "10,monitor:eDP-1"
+    ];
+  };
+
   dconf.settings."org/gnome/desktop/interface" = {
     color-scheme = "prefer-dark";
   };
