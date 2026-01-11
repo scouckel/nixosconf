7332e53b54ec49a8912333c5cef447fdc5e3c708
scouckel <james.krinsky@gmail.com>
2026-01-10 20:06:50 -0500

    zfs for shar

--- hosts/shar/configuration.nix
+++ hosts/shar/configuration.nix
@@ -33,6 +33,13 @@
   system.name = "shar";
   networking.hostName = "shar"; # Define your hostname.
 
+  boot.supportedFilesystems = [ "zfs" ];
+
+  services.zfs = {
+    autoScrub.enable = true;
+    autoSnapshot.enable = true;
+  };
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
